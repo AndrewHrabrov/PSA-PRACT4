16aa7127e687a2660429cc44ef691be64f0964ee
AndrewHrabrov <medveddimon623@gmail.com>
2024-11-15 22:31:06 +0300

    task 4

---
+++ pract4.4.py
@@ -0,0 +1,32 @@
+
+#frequencies = dict(sorted(frequencies.items())) # сортировка по алфавиту
+def char_freq(file):
+    frequencies = {}
+    russian_alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+    with open(file, encoding='utf-8') as file:
+        text = file.read().lower()
+
+        for char in text:
+            if char not in russian_alphabet:
+                text = text.replace(char, "")
+
+        for letter in text:
+            if letter in russian_alphabet:
+                if letter in frequencies:
+                    frequencies[letter] += 1
+                else:
+                    frequencies[letter] = 1
+
+    all_letters = len(text)
+
+    for letter in frequencies:
+        frequencies[letter] = round((frequencies[letter] / all_letters), 7)
+
+    # сортировка частот по убыванию
+    frequencies = dict(sorted(frequencies.items(), key=lambda item: item[1], reverse=True))
+    return frequencies
+
+frequencies = char_freq("Pract4.files/zadanie3.3.txt")
+for i in frequencies.items():
+    print(i)
